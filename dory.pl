b2058ba7baf6a2e4b96184435b75636b55667b7d
Ignacio Coluccio <ignacio.coluccio@gmail.com>
2019-08-16 08:28:06 -0300

    Template

---
+++ dory.pl
@@ -0,0 +1,11 @@
+zona(pacifico,[anemona,fosa, arrecife]).
+zona(atlantico,[bahia]).
+
+habitante(pacifico, pez(nemo, payaso, anemona)).
+habitante(pacifico, pez(marlin, payaso, anemona)).
+habitante(pacifico, pez(dory, cirujano, fosa)).
+habitante(pacifico, pajaro(becky,[pochoclo, balde, brujula])).
+habitante(atlantico, pulpo(hank,1)).
+habitante(atlantico, ballena(bailey)).
+habitante(atlantico, ballena(destiny)).
+habitante(pacifico, tortuga(crush)).
